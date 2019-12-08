cae5f582b38ac3dd5e62ef9826c9c481c6937723
milad khodabandehloo <engmiladkh1372work@gmail.com>
2019-12-08 09:56:59 +0330

    update flyweight pattern.

--- structural/fly weight.py
+++ structural/fly weight.py
@@ -1,57 +1,54 @@
-import abc
+# Instances of CheeseBrand will be the Flyweights
+class CheeseBrand(object):
+    def __init__(self, brand: str, cost: float) -> None:
+        self.brand = brand
+        self.cost = cost
+        self._immutable = True  # Disables future attributions
+
+    def __setattr__(self, name, value):
+        if getattr(self, '_immutable', False):  # Allow initial attribution
+            raise RuntimeError('This object is immutable')
+        else:
+            super().__setattr__(name, value)
+    
+
+class CheeseShop(object):
+    menu = {}  # Shared container to access the Flyweights
+    
+    def __init__(self) -> None:
+        self.orders = {}  # per-instance container with private attributes
+
+    def stock_cheese(self, brand: str, cost: float) -> None:
+        cheese = CheeseBrand(brand, cost)
+        self.menu[brand] = cheese  # Shared Flyweight
+
+    def sell_cheese(self, brand: str, units: int) -> None:
+        self.orders.setdefault(brand, 0)
+        self.orders[brand] += units  # Instance attribute
+
+    def total_units_sold(self):
+        return sum(self.orders.values())
+    
+    def total_income(self):
+        income = 0
+        for brand, units in self.orders.items():
+            income += self.menu[brand].cost * units
+        return income
 
 
-class FlyweightFactory:
-    """
-    Create and manage flyweight objects.
-    Ensure that flyweights are shared properly. When a client requests a
-    flyweight, the FlyweightFactory object supplies an existing instance
-    or creates one, if none exists.
-    """
+shop1 = CheeseShop()
+shop2 = CheeseShop()
 
-    def __init__(self):
-        self._flyweights = {}
+shop1.stock_cheese('white', 1.25)
+shop1.stock_cheese('blue', 3.75)
+# Now every CheeseShop have 'white' and 'blue' on the inventory
+# The SAME 'white' and 'blue' CheeseBrand
 
-    def get_flyweight(self, key):
-        try:
-            flyweight = self._flyweights[key]
-        except KeyError:
-            flyweight = ConcreteFlyweight()
-            self._flyweights[key] = flyweight
-        return flyweight
+shop1.sell_cheese('blue', 3)   # Both can sell
+shop2.sell_cheese('blue', 8)   # But the units sold are stored per-instance
 
+assert shop1.total_units_sold() == 3
+assert shop1.total_income() == 3.75 * 3
 
-class Flyweight(metaclass=abc.ABCMeta):
-    """
-    Declare an interface through which flyweights can receive and act on
-    extrinsic state.
-    """
-
-    def __init__(self):
-        self.intrinsic_state = None
-
-    @abc.abstractmethod
-    def operation(self, extrinsic_state):
-        pass
-
-
-class ConcreteFlyweight(Flyweight):
-    """
-    Implement the Flyweight interface and add storage for intrinsic
-    state, if any. A ConcreteFlyweight object must be sharable. Any
-    state it stores must be intrinsic; that is, it must be independent
-    of the ConcreteFlyweight object's context.
-    """
-
-    def operation(self, extrinsic_state):
-        pass
-
-
-def main():
-    flyweight_factory = FlyweightFactory()
-    concrete_flyweight = flyweight_factory.get_flyweight("key")
-    concrete_flyweight.operation(None)
-
-
-if __name__ == "__main__":
-    main()
+assert shop2.total_units_sold() == 8
+assert shop2.total_income() == 3.75 * 8
